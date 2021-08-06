ec15f7798589bd84c6afd7b9ade9add87b45ed12
yossibank <yossibank@yahoo.co.jp>
2021-08-06 21:34:29 +0900

    develop: テスト修正

--- BookManager-Swift/FirebaseKit/Tests/FirebaseKitTests/FirebaseKitTests.swift
+++ BookManager-Swift/FirebaseKit/Tests/FirebaseKitTests/FirebaseKitTests.swift
@@ -1,11 +1,4 @@
 import XCTest
 @testable import FirebaseKit
 
-final class FirebaseKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FirebaseKit().text, "Hello, World!")
-    }
-}
+final class FirebaseKitTests: XCTestCase {}
